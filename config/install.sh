c618f0f21313a54d5a4ee27d30ad43d89a5f36bf
Junwei Zhang <junwei.zhang@6wind.com>
2013-09-21 08:23:24 +0800

    install: use vim vundle
    Signed-off-by: Junwei Zhang <junwei.zhang@6wind.com>

--- config/install.sh
+++ config/install.sh
@@ -3,15 +3,18 @@ CFG_ROOT=`pwd`
 #
 git --version || sudo apt-get install git
 
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+if [ -d ~/.vim/bundle/vundle ] ; then
+	cd ~/.vim/bundle/vundle && git pull && cd -;
+else
+	git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+fi
 
 #configure vim git
-cd ~ \
-	&& ln -s $CFG_ROOT/git/gitconfig .gitconfig			\
-	&& ln -s $CFG_ROOT/git/gitcommit_template .gitcommit_template	\
-	&& ln -s $CFG_ROOT/vimrc .vimrc					\
-	&& ln -s $CFG_ROOT/vim .vim					\
-	&& cd -
+cd ~;
+ln -s $CFG_ROOT/git/gitconfig .gitconfig;
+ln -s $CFG_ROOT/git/gitcommit_template .gitcommit_template;
+ln -s $CFG_ROOT/vimrc .vimrc;
+cd -
 
 #configure bashrc
 grep junwei.bashrc ~/.bashrc || \
